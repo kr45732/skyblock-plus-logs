bbf24ca947ee1b810501999f9c5807ab7af32af7
kr45732 <52721908+kr45732@users.noreply.github.com>
2021-12-29 20:48:07 -0500

    real code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <div class="main" v-for="data in json" :key="data">
+      <message :json="data" />
+    </div>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  color: #eee;
+  background-color: #2a2a2a;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+a {
+  color: #0099ff;
+}
+
+a:hover {
+  color: #037cce;
+}
+
+.logo {
+  display: inline-block;
+  font-weight: bold;
+  font-size: 2em;
+  margin: 0;
+}
+
+.title {
+  border-bottom: 1px solid #4a4a4a;
+  padding-bottom: 0.25em;
+}
+
+.main {
+  border: none !important;
+  outline: none !important;
 }
 </style>
+
+<script>
+import Message from "./components/Message.vue";
+import * as allJson from "./json/data.json";
+
+export default {
+  data() {
+    return {
+      json: allJson.data,
+    };
+  },
+  components: { Message },
+  name: "App",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import {
+  applyPolyfills,
+  defineCustomElements
+} from "@skyra/discord-components-core/loader";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+// Tell Vue to ignore all components defined in the @skyra/discord-components-core package.
+Vue.config.ignoredElements = [/discord-[\w-]*/];
+
+// Bind the custom elements to the window object
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
